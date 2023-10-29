3d9b5c04fe734965e882f55102845ceab9704c83
prarthana.m <prarthana.m@simformsolutions.com>
2023-10-29 13:09:51 +0530

    latest code

---
+++ oct_8.py
@@ -0,0 +1,167 @@
+'''
+
+'''
+"""
+"""
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+#
+# a.print("Hello World")
+
+# a=0
+# a=1
+# print(a)
+
+# a,b=b,a
+
+# a = 5
+# print(a)
+# b=6.1
+# print(b)
+# print(a+b)
+
+
+# DATA TYPES:
+#
+# Int 1233456789
+# str "abaca123"
+# float 1.2,3.4
+# complex (1j+1)
+# list [1,"23"]
+# tuple (1,"23")
+# set  {1,2,"23"}
+# dict  {"key":"value"}
+
+#
+# #
+# #
+# #
+# #
+# #
+# #
+# print('Alpha said Beta "Gamma \\n is teasing  Delta."')
+# print('Alpha said Beta "Gamma \\t is teasing  Delta."')
+# print('Alpha said Beta "Gamma\b is teasing  Delta." a\\b ')
+#
+# print('Location of Python interpreter is: "C:\\n \\folder\\tem\\b\python\\"')
+# # # """
+# # # Escape sequence characters:
+# # # \
+# # # \n	new line
+# # # \t	tab
+# # # \b	backspace
+# # #
+# # # print("Alpha said Beta \"Gamma is teasing Delta but Gamma say I didn\'t tease Delta, now what to do?\"")
+# # #
+# print("I love India\b\b\b\b\b")
+# #
+# #
+# # #
+# # #
+# # # """
+a = 20
+b = 15
+print(a, "+", b, "=", a+b)
+#
+# name=""
+# surname=""
+# middleman=""
+# print(name,middleman,surname)
+
+# # print("Thanks!!    ", end="\t")
+# # # print("See you!")
+# # #
+# # #
+# # # Taking inputs:
+# print("Enter a: ")
+
+
+a = int(input("Enter a:"))
+print(type(a))
+b= int(input("Enter b:"))
+print(type(b))
+print(a+b)
+
+
+# b=65
+# # print(int(b))
+# int(b)
+# print(type(b))
+#
+# # # b = input("Enter b: ")
+# # # print(type(b))
+# # #
+# #
+# # # write a program that asks two numbers , a as integer and b as float values, and add both the numbers
+# # #
+# # # Enter integer number : 1
+# # # Enter float number : 1.4
+# # # Output should be like this 1 + 1.4 = 2.4
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# #
+# # """
+# # a = int(input("Enter int number:\n"))
+# # b = float(input("Enter float number:\n"))
+# # print(a, "+", b, "=", a,b)
+# # print(a, "+", b, "=", int(a) + float(b))
+# # print(5/1)
+# # """
+# #
+# #
+# # # a=1
+# # # a=2
+# # # print(a)
+# # #
+# # #
+# # # Write a Python program that asks five integers from user, saves them as a, b, c, d and e and
+# # # print their average exactly as below:
+# # #
+# # # sample execution:
+# # #
+# # # Enter 5 integers:
+# # # 1
+# # # 2
+# # # 3
+# # # 4
+# # # 5
+# # # The average of 1, 2, 3, 4 & 5 is: 3.0
+# # #
+# # #
+# # # # formatted string: f-string
+# # print("Enter 5 integers:")
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# e = int(input())
+# avg = (a + b + c + d + e)/5
+# # print("the average of ",a,b,c,d,"&",e,"=",avg)
+# print(f"The average of {a}, {b}, {c}, {d} & {e} is: {avg}")
+# # print(f"The average of {a}, {b}, {c}, {d} & {e} is:",avg)
+# url=f"{url}{domain}{search}"
+# #
+# # # HW:
+# # # take
+# # # print "My name Prarthana maheta"
+# # # """
+# # # # n = int(input("Enter how many numbers you want to find average of: "))
+# # # """
+# # # for(i = 1; i <= n; i++){
+# # # 	printf("Enter number-%d : ", i);
+# # # 	scanf("%d", &n);
+# # # }
+# # # """
+# # # i = 1
+# # # a = int(input(f"Enter number-{i}: "))
+# # #
+# # # # collections in Python: strings
